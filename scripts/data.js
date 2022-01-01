56801135236e1abe115e1a7f1590c41ac9b21fce
Saswat Kumar Pati <42134195+saswatkpati@users.noreply.github.com>
2022-01-02 02:51:27 +0530

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,126 @@
+let year = '2021';
+let semester = 'Spring';
+
+function School(code, name, courses) {
+	this.code = code;
+	this.name = name;
+	this.courses = courses;
+}
+
+function short_name(school) {
+	return school.name.split(' ').slice(2).join(' ')
+}
+
+function Course(name, nick, slot) {
+	this.name = name;
+	this.nick = nick;
+	this.slot = slot;
+}
+
+let schools = {
+	'SBS': new School('B', 'School of Biological Sciences', []),
+	'SCS': new School('C', 'School of Chemical Sciences', []),
+	'SCPS': new School('CS', 'School of Computer Sciences', []),
+	'SMS': new School('M', 'School of Mathematical Sciences', []),
+	'SPS': new School('P', 'School of Physical Sciences', []),
+	'SHSS': new School('H', 'School of Humanities and Social Sciences', []),
+};
+
+let courses = {
+
+	// always use A1, A2, ... for degenerates of slot A
+
+	/* Biology */
+	'B201': new Course('Microbiology', 'μBio', 'C'),
+	'B202': new Course('Biochemistry', 'BChem', 'F'),
+	'B203': new Course('Biophysics & Biostatistics', 'BPhy', 'A'),
+
+	'B301': new Course('Physiology I (Animal)', 'APhys', 'B'),
+	'B302': new Course('Physiology II (Plant)', 'PPhys', 'F'),
+	'B303': new Course('Ecology', 'Eco', 'E'),
+	'B354': new Course('Neurobiology', 'Neuro', 'A'),
+	'B355': new Course('Structural Biology', 'StructB', 'A'),
+
+	'B402': new Course('Developmental Biology', 'Dev Bio', 'F'),
+	'B403': new Course('Bioinformatics', 'BInfo', 'K'),
+	'B452': new Course('Genetic Engineering', 'Genetic', 'A'),
+	'B460': new Course('Virology', 'Viro', 'J'),
+
+	'B551': new Course('Advanced Molecular Biology', 'AMol', 'C'),
+	'B559': new Course('Ion Channels', 'iChan', 'I'),
+	'B562': new Course('Molecular Errors in Disease', 'merr', 'B'),
+
+	/* Chemistry */
+	'C201': new Course('Basic Inorganic Chemistry', 'IChem', 'E'),
+	'C207': new Course('Mathematical Methods for Chemists', 'MMChem', 'B'),
+	'C203': new Course('Reaction Mechanisms in Organic Chemistry', 'OChem', 'D'),
+
+	'C301': new Course('Physical Organic Chemistry', 'POChem', 'A'),
+	'C302': new Course('Molecular Spectroscopy and Group Theory', 'Spectre', 'K'),
+	'C307': new Course('Thermodynamics and Electrochemistry', 'Thermo', 'C'),
+
+	'C401': new Course('Physical Methods in Chemistry II', 'PChem II', 'J'),
+	'C403': new Course('Chemistry of Heterocycles and Natural Products', 'Hetero', 'K'),
+
+	'C560': new Course('Chemistry of Nanomaterials', 'NanoC', 'C'),
+	'C564': new Course('Theory of Molecular Spectroscopy', 'MolSpect', 'A'),
+	'C566': new Course('Catalysis', 'Cat', 'B'),
+	'C567': new Course('Advanced Main Group Chemistry', 'AdvMG', 'I'),
+	'C569': new Course('Biomacromolecules', 'BioMM', 'A'),
+
+	/* Computer Science */
+	'CS201': new Course('Theory of Computation', 'ThComp', 'I'),
+	'CS301': new Course('Design and Analysis of Algorithms', 'Algo', 'E'),
+	'CS452': new Course('Algorithmic Coding Theory', 'CG', 'B'),
+	'CS460': new Course('Machine Learning', 'ML', 'F'),
+
+	/* Humanities */
+	'H209': new Course('Speculative Fiction', 'SpecFic', 'K1'),
+	'H225': new Course('Introduction to Psychology', 'Psych', 'K2'),
+	'H235': new Course('Sociology of Science and Technology', 'Socio', 'J1'),
+	'H238': new Course('Life and Community in the Urban World', 'Urban', 'J2'),
+
+	/* Mathematics */
+	'M201': new Course('Real Analysis', 'RAnal', 'B'),
+	'M203': new Course('Discrete Mathematics', 'Discrete', 'C'),
+	'M205': new Course('Linear Algebra', 'Lin Alg', 'D'),
+
+	'M302': new Course('Rings and Modules', 'Modules', 'C'),
+	'M303': new Course('Differential Equation', 'DiffQ', 'K'),
+	'M304': new Course('Topology', 'Topo', 'B'),
+	'M305': new Course('Statistics', 'Stats', 'A'),
+	'M306': new Course('Calculus of Several Variables', 'SVC', 'D'),
+
+	'M401': new Course('Functional Analysis', 'FAnal', 'A'),
+	'M403': new Course('Commutative Algebra', 'CommA', 'C'),
+	'M451': new Course('Advanced Complex Analysis', 'ACAnal', 'A'),
+	'M453': new Course('Advanced Linear Algebra', 'ALinA', 'D'),
+	'M454': new Course('Partial Differential Equations', 'PDEs', 'I'),
+	'M463': new Course('Finite Fields', 'Field', 'D'),
+
+	/* Physics */
+	'P201': new Course('Classical Mechanics I', 'CM I', 'A'),
+	'P202': new Course('Mathematical Methods I', 'MMPhy I', 'C'),
+	'P207': new Course('Linear Optics', 'LinOpt', 'F'),
+
+	'P302': new Course('Statistical Mechanics', 'StatMech', 'I'),
+	'P303': new Course('Quantum Mechanics II', 'QM II', 'J'),
+	'P304': new Course('Special Theory of Relativity', 'STR', 'F'),
+
+	'P401': new Course('Classical Mechanics II: Mechanics of Continuous Media', 'CM II', 'E'),
+	'P451': new Course('Advanced Solid State Physics', 'ASSP', 'C'),
+	'P453': new Course('Quantum Field Theory I', 'QFT I', 'A'),
+	'P462': new Course('Introduction to Quantum Optics', 'QOpt', 'B'),
+	'P471': new Course('Quantum Information and Quantum Computation', 'QIQC', 'D'),
+	'P473': new Course('Experimental Techniques', 'Exp', 'J'),
+	'P474': new Course('Introduction to Cosmology', 'Cosmo', 'C'),
+
+};
+
+for (let course in courses) {
+	for (let school in schools) {
+		if (schools[school].code === course.match(/^[^0-9]*/)[0]) {
+			schools[school].courses.push(course);
+		}
+	}
+}
---
+++ scripts/main.js
@@ -0,0 +1,130 @@
+let courseUl = document.querySelector('#course-list');
+let schoolTabs = document.querySelector('#school-tabs');
+let schoolList = document.querySelector('#school-list');
+
+/* Generating the DOM tree containing the schools
+ * and the list of courses */
+for (let school in schools) {
+	let schoolTab = document.createElement('button');
+	schoolTab.textContent = school;
+	schoolTab.addEventListener('click', function() { 
+		document.querySelectorAll('.school').forEach(function(b) { b.classList.add('hidden') });
+		let schoolCourses = document.querySelector('#' + school);
+		schoolCourses.classList.remove('hidden');
+		schoolCourses.classList.add('visible');
+	});
+	schoolTabs.appendChild(schoolTab);
+
+	let schoolDiv = document.createElement('div');
+	schoolDiv.classList.add('school');
+	school !== 'SBS' ? schoolDiv.classList.add('hidden') : null;
+	schoolDiv.setAttribute('id', school);
+
+	let schoolHeader = document.createElement('h3');
+	schoolHeader.textContent = schools[school].name;
+
+	let schoolUl =  document.createElement('ul');
+	schoolList.appendChild(schoolDiv);
+	schoolDiv.appendChild(schoolHeader);
+	schoolDiv.appendChild(schoolUl);
+
+	schools[school].courses.forEach(function(course) {
+		let slot = courses[course].slot;
+
+		let courseLabel = document.createElement('label');
+		courseLabel.setAttribute('for', course);
+		courseLabel.textContent = course;
+		courseLabel.textContent += ": " + courses[course].name;
+
+		let courseLi = document.createElement('li');
+		courseLi.classList.add('course');
+
+		let courseCheckBox = document.createElement('input');
+		courseCheckBox.setAttribute('type', 'checkbox');
+		courseCheckBox.setAttribute('value', course);
+		courseCheckBox.setAttribute('id', course);
+
+		schoolUl.appendChild(courseLi);
+		courseLi.appendChild(courseCheckBox);
+		courseLi.appendChild(courseLabel);
+	});
+}
+
+/* A function that cleans the cells of the timetable */
+function clean() {
+	let slotsInTimetable = document.querySelectorAll('td:not(.lunch)');
+
+	slotsInTimetable.forEach(function(slot) {
+		slot.textContent = '';
+	})
+}
+
+/*to deal with conflicts like A and A1*/
+function slotCompare(currentc, newc) {
+	 if(currentc.length != newc.length) return currentc.charAt(0) === newc.charAt(0)
+	 return currentc === newc
+}
+
+/* Replaces the slots in the timetable with the selected
+ * course in that slot */
+function generate(nick) {
+	clean();
+	let courseCheckBoxes = document.querySelectorAll('input');
+	let occupiedSlots = []
+	courseCheckBoxes.forEach(function(course) {
+		if (course.checked) {
+			let courseCode = course.getAttribute('id');
+			let courseSlot = courses[courseCode].slot;
+			for (let i = 0; i < occupiedSlots.length; ++i) {
+				if (slotCompare(occupiedSlots[i], courseSlot)) {
+					alert("Schedule conflict detected. Course selection may need a modification.");
+					location = location;
+				}
+			}
+
+			occupiedSlots.push(courseSlot);
+
+			let courseSlotInTimetable = document.querySelectorAll('td.' + courseSlot);
+			courseSlotInTimetable.forEach(function(slotInTimetable) {
+				slotInTimetable.textContent = nick ? courses[courseCode].nick : courseCode;
+			})
+		}
+	})
+	occupiedSlots = [];
+}
+
+function generatePdf() {
+	let doc = new jsPDF({
+		orientation: 'landscape',
+		unit: 'pt'
+	})
+
+	let timetable = document.querySelector('table');
+	let jsonTable = doc.autoTableHtmlToJson(timetable);
+	doc.text('Timetable for Fall Semester 2021', 300, 25);
+	doc.autoTable(jsonTable.columns, jsonTable.data, {
+		styles: {cellPadding: 10,
+				 fontSize: 12,
+				 lineColor: 10,
+				 lineWidth: .5,
+				 overflow: 'linebreak',
+				 halign: 'center',
+				 valign: 'middle'},
+		theme: 'plain'});
+
+	doc.save("tt_fall_2021.pdf");
+}
+
+let generateCodeButton = document.querySelector('button#gen-code');
+generateCodeButton.addEventListener('click', function() { generate(false) });
+
+let generateNickButton = document.querySelector('button#gen-nick');
+generateNickButton.addEventListener('click', function() { generate(true) });
+
+let pdfButton = document.querySelector('button#pdf');
+pdfButton.onclick = generatePdf;
+
+let resetButton = document.querySelector('button#reset');
+resetButton.onclick = function() {
+	location = location;
+}
